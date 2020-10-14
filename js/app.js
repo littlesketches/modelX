f1e92e633b9bc8ebfb6a3638c2cab2a14006a5d3
littlesketches <brendan_acn@hotmail.com>
2020-10-14 22:37:20 +1100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,9 +4,7 @@
 /****
 /***********************************/
 
-
-
-    console.log('BUILDING APP...')
+console.log('BUILDING APP...')
 
 
 const settings = {
@@ -45,13 +43,22 @@ const settings = {
 const state = {
     timeLoaded:         new Date(),
     time:               {},
+    modelTime:          {
+        hour: 0
+    },
     environment: {
         windSpeed:      10, 
         windDirection:  0,
         temperature:    25
     }
-
 }
+
+const simulation = {
+    dayLength: 60000
+}
+
+
+
 
 // Set current time to now
     state.time.year = state.timeLoaded.getFullYear()
@@ -117,7 +124,6 @@ const activitySchema = {
 
 
 
-
 const palette = {
     A: {
         '01': 	'#FFA8CB',
@@ -137,3 +143,83 @@ const visualModel = {
 
     }
 }
+
+
+////////////
+
+
+settings.lights = {
+    intensity: {
+        ambientIntensity: {
+            midday: 1.2,
+            midnight: 0
+        }
+    },
+
+    byHour: {
+        sunIntensity: [ 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 
+                        0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 
+                        0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 
+                        0.8, 0.8, 0.8, 0.8, 0.8, 0.8],
+
+        hemiIntensity:[ 0.0, 0.0, 0.0, 0.0, 0.2, 0.4, 
+                        0.6, 0.8, 0.8, 0.8, 0.8, 0.8, 
+                        0.8, 0.8, 0.8, 0.8, 0.8, 0.6, 
+                        0.4, 0.2, 0.0, 0.0, 0.0, 0.0],
+
+        color:        [ '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', 
+                        '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', 
+                        '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', 
+                        '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa'],  
+
+        groundColor: [ '#454545', '#454545', '#454545', '#454545', '#454545', '#454545', 
+                        '#454545', '#454545', '#454545', '#454545', '#454545', '#454545', 
+                        '#454545', '#454545', '#454545', '#454545', '#454545', '#454545', 
+                        '#454545', '#454545', '#454545', '#454545', '#454545', '#454545']
+    }
+}
+
+settings.evironment = {
+    hotEvening: {
+            'sky-top': '#f99058', 
+            'sky-bottom': '#f4fdaf',
+            'fog-type':'linear', 
+            'fog-density': 0, 
+            'fog-col': '#fff',
+            'water-col': 'aqua',
+        },
+        desert: {
+            'sky-top':      '#fff', 
+            'sky-bottom':   '#d78d37', 
+            'fog-type':     'exponetial', 
+            'fog-density':  0.01, 
+            'fog-col':      '#8c4a0d',
+            'fog-far':      200,
+            'fog-near':     0,
+        },
+
+    }
+
+
+
+function initDOMsettings(){
+
+document.getElementById('sunriseMixin').getAttribute('animation__sunrise')
+// document.getElementById('sunriseMixin').setAttribute('animation__sunrise', "property: light.intensity; from: 0; to: 1.5; dur: 15000; autoplay: false")
+    // document.getElementById('sunriseMixin').setAttribute('animation__sunrise', {
+    //     from:       settings.lights.intensity.ambientIntensity.midnight,
+    //     to:         settings.lights.intensity.ambientIntensity.midday,  
+    //     dur:        simulation.dayLength , 
+    //     autoplay:   false
+    // })
+
+
+    // document.getElementById('sunsetMixin').setAttribute('animation__sunrise', {
+    //     to:         settings.lights.intensity.ambientIntensity.midnight,
+    //     from:       settings.lights.intensity.ambientIntensity.midday,  
+    //     dur:        simulation.dayLength,
+    //     autoplay:   false
+    // })
+
+}
+
--- js/components.js
+++ js/components.js
@@ -1,29 +1,16 @@
 
 console.log('REGISTERING A-FRAME COMPONENTS...')
 
+    AFRAME.registerComponent('setup', {
+        init: function(){
+                console.log('****SETTING DOM ELEMENTS UP...****')
 
-// settings.lights = {
-//     sunIntensity: [ 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 
-//                     0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 
-//                     0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 
-//                     0.8, 0.8, 0.8, 0.8, 0.8, 0.8],
+            console.log(document.getElementById('sunriseMixin'))
+        }
+    })
 
-//     hemiIntensity:[ 0.0, 0.0, 0.0, 0.0, 0.2, 0.4, 
-//                     0.6, 0.8, 0.8, 0.8, 0.8, 0.8, 
-//                     0.8, 0.8, 0.8, 0.8, 0.8, 0.6, 
-//                     0.4, 0.2, 0.0, 0.0, 0.0, 0.0],
 
-//     color:        [ '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', 
-//                     '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', 
-//                     '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', 
-//                     '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa', '#f6e3fa'],  
 
-//     groundColor: [ '#454545', '#454545', '#454545', '#454545', '#454545', '#454545', 
-//                     '#454545', '#454545', '#454545', '#454545', '#454545', '#454545', 
-//                     '#454545', '#454545', '#454545', '#454545', '#454545', '#454545', 
-//                     '#454545', '#454545', '#454545', '#454545', '#454545', '#454545']
-
-// }
 
 
     AFRAME.registerComponent('ortho', {
@@ -62,33 +49,80 @@ console.log('REGISTERING A-FRAME COMPONENTS...')
 
     AFRAME.registerComponent("position-sun", {        
         init: function() {
-            console.log('Setting the sun path..')
-            // Create sun curve path
-            const pointIDs =["sun-0000", "sun-0100", "sun-0200", "sun-0300", "sun-0400", "sun-0500", "sun-0600", "sun-0700", "sun-0800", "sun-0900", "sun-1000", "sun-1100", "sun-1200", "sun-1300", "sun-1400", "sun-1500", "sun-1600", "sun-1700", "sun-1800", "sun-1900", "sun-2000", "sun-2100", "sun-2200", "sun-2300"],
-                indexToNow = Math.round(state.time.hour +(state.time.minutes/60)) 
-            let origin
+            // console.log('Setting the sun path..')
+            // // Create sun curve path
+            // const pointIDs =["sun-0000", "sun-0100", "sun-0200", "sun-0300", "sun-0400", "sun-0500", "sun-0600", "sun-0700", "sun-0800", "sun-0900", "sun-1000", "sun-1100", "sun-1200", "sun-1300", "sun-1400", "sun-1500", "sun-1600", "sun-1700", "sun-1800", "sun-1900", "sun-2000", "sun-2100", "sun-2200", "sun-2300"],
+            //     indexToNow = Math.round(state.time.hour +(state.time.minutes/60)) 
+            // let origin
+            // pointIDs.forEach((id, i) => {
+            //     const pos = document.getElementById(id).getAttribute('position')
+            //     pos.x = settings.environment.sunX[state.time.season]
+            //     document.getElementById(id).setAttribute('position', pos)
+ 
+            //     const pathToNowEl = document.getElementById('sun-path-pointsToNow')
+            //     if(i <=  indexToNow){                
+            //         const el = document.createElement('a-curve-point')
+            //         el.setAttribute('position', pos)
+            //         pathToNowEl.appendChild(el)  
+            //    } 
+            // })
+
+            // if(indexToNow === 0){
+            //     document.getElementById('sun-body').setAttribute('position', document.getElementById(pointIDs[0]).getAttribute('position'))
+            //     document.getElementById('sun-light').setAttribute('position', document.getElementById(pointIDs[0]).getAttribute('position'))
+            // } else {
+            //     document.getElementById('sun-body').setAttribute('alongpath', "curve: #sun-path-pointsToNow ; loop: false; dur: 20000; resetonplay: true " )
+            //     document.getElementById('sun-light').setAttribute('alongpath', "curve: #sun-path-pointsToNow ; loop: false; dur: 20000; resetonplay: true " )
+
+            // }
+            // Add animation along path for sun body and sun direcitonal light
+        }
+    })
+
+
+
+
+    AFRAME.registerComponent("cycle-sun", {
+        schema: {
+            dur:                {type: 'number',   default: simulation.dayLength},
+            hour:               { type: 'int',      default: 0}
+        },
+
+        init: function() {
+            console.log(`Initiating the sun cycle of ${this.data.dur / 1000 } seconds`)
+            const pointIDs = ["sun-0000", "sun-0100", "sun-0200", "sun-0300", "sun-0400", "sun-0500", "sun-0600", "sun-0700", "sun-0800", "sun-0900", "sun-1000", "sun-1100", "sun-1200", "sun-1300", "sun-1400", "sun-1500", "sun-1600", "sun-1700", "sun-1800", "sun-1900", "sun-2000", "sun-2100", "sun-2200", "sun-2300"],
+                sunBody =           document.getElementById('sun-body'),
+                sunLight =          document.getElementById('sun-light')
+
+            // Create sun curve pathm from midnight
             pointIDs.forEach((id, i) => {
                 const pos = document.getElementById(id).getAttribute('position')
                 pos.x = settings.environment.sunX[state.time.season]
                 document.getElementById(id).setAttribute('position', pos)
- 
-                const pathToNowEl = document.getElementById('sun-path-pointsToNow')
-                if(i <=  indexToNow){                
-                    const el = document.createElement('a-curve-point')
-                    el.setAttribute('position', pos)
-                    pathToNowEl.appendChild(el)  
-               } 
             })
 
-            if(indexToNow === 0){
-                document.getElementById('sun-body').setAttribute('position', document.getElementById(pointIDs[0]).getAttribute('position'))
-                document.getElementById('sun-light').setAttribute('position', document.getElementById(pointIDs[0]).getAttribute('position'))
-            } else {
-                document.getElementById('sun-body').setAttribute('alongpath', "curve: #sun-path-pointsToNow ; loop: false; dur: 20000; resetonplay: true " )
-                document.getElementById('sun-light').setAttribute('alongpath', "curve: #sun-path-pointsToNow ; loop: false; dur: 20000; resetonplay: true " )
+            // Add animation along path for sun body and sun direcitonal light, starting at midnight
+            sunBody.setAttribute('alongpath', `curve: #sun-path-points; loop: true; dur: ${this.data.dur}; resetonplay: true ` )
+            sunLight.setAttribute('alongpath', `curve: #sun-path-points ; loop: true; dur: ${this.data.dur}; resetonplay: true ` )
+        },
 
-            }
+        pause: function () {
+            clearInterval(state.modelTime.timer)
+            console.log('Model clock stopped at '+state.modelTime.hour)
+        },
 
-            // Add animation along path for sun body and sun direcitonal light
+        play: function () {
+            const sunLight =        document.getElementById('sun-light'),
+                sky =               document.getElementById('sky'),
+                ambientLight =      document.getElementById('ambient-light')
+
+
+            // Start the hour of day timer
+            state.modelTime.hour = 0
+            state.modelTime.timer = setInterval( () => { 
+                state.modelTime.hour = (state.modelTime.hour + 1) % 24  
+                // console.log('Hour is '+state.modelTime.hour )
+            }, this.data.dur / 24 )
         }
+
     })
